26da48541a00771052d9da92f8d83b4cd4d5eaee
Teste <otavianomattos10@gmail.com>
2022-12-16 20:22:11 -0400

    feat: added docker compose into the project

--- nestjs-monorepo-docker/apps/kanbam/src/repositories/generic.repository.ts
+++ nestjs-monorepo-docker/apps/kanbam/src/repositories/generic.repository.ts
@@ -1,58 +1,49 @@
 import { UserSchema } from './../entities/user.entity';
-import { HttpException, Injectable } from "@nestjs/common";
-import * as Realm from "realm";
-import { TaskSchema } from "src/entities/task.entity";
+import { HttpException, Injectable } from '@nestjs/common';
+import * as Realm from 'realm';
+import { TaskSchema } from '../entities/task.entity';
 
 /* istanbul ignore next */
 @Injectable()
 export class GenericRepository {
+  private realm: Realm;
+  private app = new Realm.App({ id: 'kanban-teupx' });
+  constructor() {}
 
-    private realm: Realm;
-    private app = new Realm.App({ id: "kanban-teupx" });
-    constructor(){}
+  async getConnectionRealm(): Promise<boolean> {
+    const credentials = Realm.Credentials.anonymous();
+    try {
+      //Caso o usuario esteja conectado, ele encerra a conexao
+      //this.app.currentUser?.logOut();
+      if (!this.app.currentUser) {
+        await this.app.logIn(credentials);
+      }
 
-    async getConnectionRealm(): Promise<boolean> {
-		
-        const credentials = Realm.Credentials.anonymous();
-        try {                    
-            
-            //Caso o usuario esteja conectado, ele encerra a conexao
-            //this.app.currentUser?.logOut();
-            if (!this.app.currentUser) {
-                await this.app.logIn(credentials);   
-            }            
-            
-            return await Boolean(this.app.currentUser);
-            
-        } catch (error) {
-            throw new HttpException(error.message, error.code);
-        }
+      return await Boolean(this.app.currentUser);
+    } catch (error) {
+      throw new HttpException(error.message, error.code);
+    }
+  }
+
+  async closeConnectionRealm(): Promise<void> {
+    //const app = new Realm.App({ id: "peccode-coletor-backend-gijfu" });
+    //Caso o usuario esteja conectado, ele encerra a conexao
+    //app.currentUser?.logOut();
+    //Desconecto o Realm.
+    //this.realm.close();
+  }
+
+  async openConnectionRealm(): Promise<Realm> {
+    if (await this.getConnectionRealm()) {
+      this.realm = await Realm.open({
+        schema: this.getSchemas(),
+      });
     }
 
-    async closeConnectionRealm(): Promise<void> {
+    return this.realm;
+  }
 
-		//const app = new Realm.App({ id: "peccode-coletor-backend-gijfu" });
-
-        //Caso o usuario esteja conectado, ele encerra a conexao
-        //app.currentUser?.logOut();
-
-        //Desconecto o Realm.
-        //this.realm.close();
-
-    }
-
-    async openConnectionRealm(): Promise<Realm> {
-
-        if(await this.getConnectionRealm()){
-            this.realm = await Realm.open({
-                schema: this.getSchemas()
-            });
-        }
-
-        return this.realm;            
-    }
-
-    private getSchemas() : Array<Realm.ObjectSchema>{
-        return[TaskSchema, UserSchema];
-    }
+  private getSchemas(): Array<Realm.ObjectSchema> {
+    return [TaskSchema, UserSchema];
+  }
 }
--- nestjs-monorepo-docker/apps/kanbam/src/repositories/user.repository.ts
+++ nestjs-monorepo-docker/apps/kanbam/src/repositories/user.repository.ts
@@ -1,59 +1,59 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { IRepository } from '../interface/IRepository.interface';
 import { GenericRepository } from './generic.repository';
-import { User, UserSchema } from 'src/entities/User.entity';
-import { v4 as uuidv4} from 'uuid';
-import { IUser } from 'src/interface/IUser.interface';
+import { User, UserSchema } from '../entities/user.entity';
+import { v4 as uuidv4 } from 'uuid';
+import { IUser } from '../interface/IUser.interface';
 
 /* istanbul ignore next */
 @Injectable()
 export class UserRepository extends GenericRepository implements IRepository {
   async create(data: IUser): Promise<string> {
     try {
-			const realm: Realm = await super.openConnectionRealm();
-			const id: string = uuidv4();
-      		// const userId: string = uuidv4();
+      const realm: Realm = await super.openConnectionRealm();
+      const id: string = uuidv4();
+      // const userId: string = uuidv4();
 
-			realm.write(() => {
-				realm.create<User>("User", {
-					_id: id,
-					email: data.email,
-					password: data.password,
-					enable: true,
-				});
-			  });	
-			  
-			super.closeConnectionRealm();
+      realm.write(() => {
+        realm.create<User>('User', {
+          _id: id,
+          email: data.email,
+          password: data.password,
+          enable: true,
+        });
+      });
 
-			return `{ "code": ${HttpStatus.CREATED},  "id": "${id}" }`;
-		} catch (error) {
-			throw new HttpException(error.message, error.code);
-		}	
+      super.closeConnectionRealm();
+
+      return `{ "code": ${HttpStatus.CREATED},  "id": "${id}" }`;
+    } catch (error) {
+      throw new HttpException(error.message, error.code);
+    }
   }
 
   async update(data: IUser): Promise<string> {
-    try {			
-		const realm: Realm = await super.openConnectionRealm();
+    try {
+      const realm: Realm = await super.openConnectionRealm();
 
-		const User = realm.objects<User>("User");
-		const existUser = User.filtered(`_id = '${data.id}'`)[0];
+      const User = realm.objects<User>('User');
+      const existUser = User.filtered(`_id = '${data.id}'`)[0];
 
-		if(Boolean(existUser)) {
-			realm.write(() => {					
-				existUser.enable = data.enable
-				existUser.password = data.password
-			});	
-		} else {
-			super.closeConnectionRealm();
-			return `{ "code": ${HttpStatus.NOT_FOUND},  "message": "registro não encontrado." }`;
-		}
-		  
-		super.closeConnectionRealm();
+      if (Boolean(existUser)) {
+        realm.write(() => {
+          existUser.enable = data.enable;
+          existUser.password = data.password;
+        });
+      } else {
+        super.closeConnectionRealm();
+        return `{ "code": ${HttpStatus.NOT_FOUND},  "message": "registro não encontrado." }`;
+      }
 
-		return JSON.stringify(data);
-	} catch (error) {
-		throw new HttpException(error.message, error.code);
-	}
+      super.closeConnectionRealm();
+
+      return JSON.stringify(data);
+    } catch (error) {
+      throw new HttpException(error.message, error.code);
+    }
   }
 
   async delete(data: IUser): Promise<string> {
@@ -62,48 +62,53 @@ export class UserRepository extends GenericRepository implements IRepository {
 
   async getAll(): Promise<string> {
     try {
-		const realm: Realm = await super.openConnectionRealm();
-		const country = realm.objects<User>("User");
-		const results = JSON.stringify(country.sorted("_id"), null, 2);
+      const realm: Realm = await super.openConnectionRealm();
+      const country = realm.objects<User>('User');
+      const results = JSON.stringify(country.sorted('_id'), null, 2);
 
-		super.closeConnectionRealm();
-		
-		return results;
-	} catch (error) {
-		throw new HttpException(error.message, error.code);
-	}
+      super.closeConnectionRealm();
+
+      return results;
+    } catch (error) {
+      throw new HttpException(error.message, error.code);
+    }
   }
 
   async getById(id: string): Promise<string> {
     try {
-			const realm: Realm = await super.openConnectionRealm();
-			const breed = realm.objects<User>("User");			
-			const results = JSON.stringify(breed.filtered(`_id = '${id}'`), null, 2);
+      const realm: Realm = await super.openConnectionRealm();
+      const breed = realm.objects<User>('User');
+      const results = JSON.stringify(breed.filtered(`_id = '${id}'`), null, 2);
 
-			super.closeConnectionRealm();
-			
-			return results;
-		} catch (error) {
-			throw new HttpException(error.message, error.code);
-		}
+      super.closeConnectionRealm();
+
+      return results;
+    } catch (error) {
+      throw new HttpException(error.message, error.code);
+    }
   }
 
   async signin(email: string, password: string): Promise<string> {
     try {
-			const realm: Realm = await super.openConnectionRealm();
-			const breed = realm.objects<User>("User");			
-			const results = JSON.stringify(breed.filtered(`email = '${email}' && password='${password}' && enable = true`), null, 2);
+      const realm: Realm = await super.openConnectionRealm();
+      const breed = realm.objects<User>('User');
+      const results = JSON.stringify(
+        breed.filtered(
+          `email = '${email}' && password='${password}' && enable = true`,
+        ),
+        null,
+        2,
+      );
 
-			super.closeConnectionRealm();
+      super.closeConnectionRealm();
 
-			if(results == "[]")
-			{
-				throw new HttpException("login failed", 401);	
-			}
-			
-			return results;
-		} catch (error) {
-			throw new HttpException(error.message, error.code);
-		}
+      if (results == '[]') {
+        throw new HttpException('login failed', 401);
+      }
+
+      return results;
+    } catch (error) {
+      throw new HttpException(error.message, error.code);
+    }
   }
 }
